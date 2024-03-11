91cbaab3e4a9bb809fe1e606020e1194036afd80
Taushif Habiboellah <taushif.habiboellah@gmail.com>
2024-03-11 13:13:52 +0100

    add base

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const Index = () => {
+
+    return (
+        <div>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+export default Index
